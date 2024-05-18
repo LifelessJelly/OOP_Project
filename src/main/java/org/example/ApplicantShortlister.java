c010dc530f102835e2b934b4b535e594a79d28f2
josep <joseph_chiu@outlook.com>
2024-05-18 23:15:09 +0800

    -Added applicant shortlister class (WIP)

---
+++ src/main/java/org/example/ApplicantShortlister.java
@@ -0,0 +1,11 @@
+package org.example;
+
+
+import Subsystems.Models;
+
+public class ApplicantShortlister {
+    Models.ApplicantModel applicant;
+
+    ApplicantShortlister(Models.ApplicantModel applicant) {
+    }
+}
