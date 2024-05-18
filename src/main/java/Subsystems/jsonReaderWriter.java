45fbe0cdbde4019c6dacae29d286970c0e501979
josep <joseph_chiu@outlook.com>
2024-05-18 23:13:11 +0800

    -Added JSON read write helper class

---
+++ src/main/java/Subsystems/jsonReaderWriter.java
@@ -0,0 +1,16 @@
+package Subsystems;
+
+import com.google.gson.Gson;
+
+public class jsonReaderWriter {
+    private static final Gson gson = new Gson();
+
+    public static <T> T jsonToModel(String json, Class<T> userType){
+        return gson.fromJson(json, userType);
+    }
+    public static <T> String modelToJson(Class<T> userType){
+        return gson.toJson(userType);
+    }
+}
+
+
