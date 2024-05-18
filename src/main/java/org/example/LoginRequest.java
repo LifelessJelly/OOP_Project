6723588975695a385f5dc543a23eb4b642a2322c
josep <joseph_chiu@outlook.com>
2024-05-18 23:11:56 +0800

    -Replaced the database iteration due to removal of the database class (WIP)

--- src/main/java/org/example/LoginRequest.java
+++ src/main/java/org/example/LoginRequest.java
@@ -1,12 +1,39 @@
 package org.example;
 
 import Subsystems.Encryptor;
+import deprecate.HRDataBase;
 
 import java.security.NoSuchAlgorithmException;
 
+
+/**
+ * Class LoginRequest
+ * Handles login request from the frontend login page
+ * <p>
+ * LoginRequest(String username, String password, String domain)
+ * Takes in username and password of the admin employee, and the domain ("HR" or "MANAGER") to access the specified database
+ * [usage]
+ * new LoginRequest("Bentillo John Matthew Reales", "PINOY_pride@123", "HR").isLoginSuccessful()
+ */
+// Redo this class
 public class LoginRequest {
-    boolean loginSuccess;
-    LoginRequest(String username, String password, HRDataBase dataBase) throws NoSuchAlgorithmException {
-        loginSuccess = dataBase.matchCredentials(new Encryptor(username), new Encryptor(password));
+    public boolean loginSuccess;
+    LoginRequest(String username, String password, String domain) throws NoSuchAlgorithmException {
+        // Fetches the encrypted passwords
+        switch (domain) {
+            case "HR":
+                //code
+                break;
+            case "MANAGER":
+                // more code
+                break;
+
+
+
+        }
+    }
+
+    public boolean isLoginSuccessful() {
+        return loginSuccess;
     }
 }
