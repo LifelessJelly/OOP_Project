a308bfc442baf0d158622c695786cbfe997a5ec2
josep <joseph_chiu@outlook.com>
2024-05-19 23:32:02 +0800

    -Corrected wrong implementation -Now uses a constructor and an ApplicantModel object to store captured data

--- src/main/java/org/example/ViewApplicantsBackend.java
+++ src/main/java/org/example/ViewApplicantsBackend.java
@@ -4,9 +4,21 @@ package org.example;
 import Subsystems.Models;
 import Subsystems.jsonReaderWriter;
 
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 public class ViewApplicantsBackend {
-    Models.ApplicantModel retrieveApplicantsData(String pathToApplicantsFile){
-        return jsonReaderWriter.jsonToModel(pathToApplicantsFile, Models.ApplicantModel.class);
+
+    private Models.ApplicantModel data;
+
+    ViewApplicantsBackend(String pathToApplicantsFile) throws IOException {
+        data = jsonReaderWriter.jsonToModel(Files.readString(Paths.get(pathToApplicantsFile)), Models.ApplicantModel.class);
+
 
     }
+
 }
