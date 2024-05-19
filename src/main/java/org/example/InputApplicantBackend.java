e7bada13dff255c9560ca68ea8d4ac57dfe2f638
josep <joseph_chiu@outlook.com>
2024-05-19 23:28:17 +0800

    Added methods to construct a new applicant straight from the InputApplicantBackend class

--- src/main/java/org/example/InputApplicantBackend.java
+++ src/main/java/org/example/InputApplicantBackend.java
@@ -3,7 +3,9 @@ package org.example;
 import Subsystems.Models;
 import Subsystems.jsonReaderWriter;
 
+import java.awt.*;
 import java.util.ArrayList;
+import java.util.Date;
 
 public class InputApplicantBackend {
 
@@ -18,12 +20,42 @@ public class InputApplicantBackend {
         applicants.add(applicant);
     }
 
+    public void addApplicant(String name,
+                             Date birthdate,
+                             int age,
+                             String nationality,
+                             String gender,
+                             String NRIC_Fin_Passport,
+                             Image applicantImage,
+                             String resume,
+                             ArrayList<Models.ApplicantModel.ApplicantPreviousJobExperience> jobExperience){
+
+        addApplicant(name, birthdate.getTime(), age, nationality, gender, NRIC_Fin_Passport, applicantImage, resume, jobExperience);
+    }
+
+    public void addApplicant(String name,
+                             long birthdate,
+                             int age,
+                             String nationality,
+                             String gender,
+                             String NRIC_Fin_Passport,
+                             Image applicantImage,
+                             String resume,
+                             ArrayList<Models.ApplicantModel.ApplicantPreviousJobExperience> jobExperience) {
+
+        Models.ApplicantModel.Applicant applicant = new Models.ApplicantModel.Applicant();
+        applicant.setApplicantDetails(name, birthdate, age, nationality, gender, NRIC_Fin_Passport, applicantImage);
+        applicant.setResume(resume);
+        applicants.add(applicant);
+    }
+
     public void addApplicantFromJson(String applicantJson) {
         applicants.add(jsonReaderWriter.jsonToModel(applicantJson, Models.ApplicantModel.Applicant.class));
     }
 
     public void commitAndPushAllApplicants(){
         ptrToArray.addApplicant(applicants);
+        System.gc(); // Calls garbage collector
     }
 
 }
