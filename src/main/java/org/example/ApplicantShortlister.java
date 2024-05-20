e396f5dafec86645dc9f5a8e06e02809393dbd15
josep <joseph_chiu@outlook.com>
2024-05-20 23:04:04 +0800

    -Change import

--- src/main/java/org/example/ApplicantShortlister.java
+++ src/main/java/org/example/ApplicantShortlister.java
@@ -1,14 +1,15 @@
 package org.example;
 
 
+import Subsystems.ApplicantModel;
 import Subsystems.Models;
 
-import java.util.ArrayList;
+import java.util.List;
 
 public class ApplicantShortlister {
     //Shortlists multiple applicants at once
-    ArrayList<Models.ApplicantModel.Applicant> applicantShortlistToBeChanged;
-    public ApplicantShortlister(int[] applicantIndices, Models.ApplicantModel applicants){
+    List<ApplicantModel.Applicant> applicantShortlistToBeChanged;
+    public ApplicantShortlister(int[] applicantIndices, ApplicantModel applicants){
         applicantShortlistToBeChanged = applicants.getApplicantsByIndex(applicantIndices);
         for (var a : applicantShortlistToBeChanged) {
             a.toggleApplicantShortlist();
