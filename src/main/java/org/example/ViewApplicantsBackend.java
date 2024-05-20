8e6dc9a840ae6352f9bdf1a630f8bf883c77f8c8
josep <joseph_chiu@outlook.com>
2024-05-20 23:27:36 +0800

    -Changed import

--- src/main/java/org/example/ViewApplicantsBackend.java
+++ src/main/java/org/example/ViewApplicantsBackend.java
@@ -1,6 +1,7 @@
 package org.example;
 
 
+import Subsystems.ApplicantModel;
 import Subsystems.Models;
 import Subsystems.jsonReaderWriter;
 
@@ -13,10 +14,10 @@ import java.nio.file.Paths;
 
 public class ViewApplicantsBackend {
 
-    private Models.ApplicantModel data;
+    private ApplicantModel data;
 
     ViewApplicantsBackend(String pathToApplicantsFile) throws IOException {
-        data = jsonReaderWriter.jsonToModel(Files.readString(Paths.get(pathToApplicantsFile)), Models.ApplicantModel.class);
+        data = jsonReaderWriter.jsonToModel(Files.readString(Paths.get(pathToApplicantsFile)), ApplicantModel.class);
 
 
     }
