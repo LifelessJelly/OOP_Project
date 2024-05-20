9580529467da93025a357042a62f0dd543bb46ef
josep <joseph_chiu@outlook.com>
2024-05-20 23:26:17 +0800

    -Example Model to JSON conversion

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,22 +1,22 @@
 package org.example;
 
-import com.google.gson.Gson;
+import Subsystems.ApplicantModel;
+import Subsystems.jsonReaderWriter;
 
 import javax.swing.*;
-import java.awt.*;
-import java.io.File;
-import java.io.FileNotFoundException;
 
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Date;
-import java.util.List;
-import java.util.StringTokenizer;
 
-import static java.awt.Color.GRAY;
 
 public class Main extends JFrame {
-    public static void main(String[] args) throws IOException {
-
+    public static void main(String[] args) {
+        ApplicantModel applicantModel = new ApplicantModel();
+        InputApplicantBackend inputApplicantBackend = new InputApplicantBackend(applicantModel);
+        inputApplicantBackend.addApplicant("John", new Date(), 25, "Singapore", "Bisexual", "T000000000000", null, "I like kids", new ArrayList<>());
+        inputApplicantBackend.commitAndPushAllApplicants();
+        System.out.println(applicantModel);
+        String json = jsonReaderWriter.modelToJson(applicantModel);
+        System.out.println(json);
     }
 }
