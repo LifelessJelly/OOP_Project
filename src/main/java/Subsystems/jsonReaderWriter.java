d8fa87884a353a565c5ba5b3cd82c3a9eab61fd9
josep <joseph_chiu@outlook.com>
2024-05-22 11:44:21 +0800

    -Changed jsonToModel to be null-safe if an empty string is passed in

--- src/main/java/Subsystems/jsonReaderWriter.java
+++ src/main/java/Subsystems/jsonReaderWriter.java
@@ -1,15 +1,37 @@
 package Subsystems;
 
 import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonSyntaxException;
+
+import java.lang.reflect.InvocationTargetException;
 
 public class jsonReaderWriter {
     private static final Gson gson = new Gson();
 
-    public static <T> T jsonToModel(String json, Class<T> userType){
-        return gson.fromJson(json, userType);
+    public static <T> T jsonToModel(String json, Class<T> userType) {
+        T t;
+        t = gson.fromJson(json, userType);
+        if (t == null){
+            try {
+                t = userType.getDeclaredConstructor().newInstance();
+            } catch (InstantiationException | IllegalAccessException | InvocationTargetException |
+                     NoSuchMethodException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        return t;
     }
     public static <T> String modelToJson(T userType){
         return gson.toJson(userType);
+    }
+}
+
+class Reflector<T> {
+    private final Class<T> reflection;
+    public Reflector(Class<T> reflection) {
+        this.reflection = reflection;
     }
 }
 
