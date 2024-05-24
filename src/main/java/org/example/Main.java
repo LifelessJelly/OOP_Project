0a8d7836d76c0210918e810ae926cb877663dbe1
josep <joseph_chiu@outlook.com>
2024-05-24 10:30:42 +0800

    -Testing

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,7 @@
 package org.example;
 
 import Subsystems.ApplicantModel;
+import Subsystems.Pointer;
 import Subsystems.jsonReaderWriter;
 
 import javax.swing.*;
@@ -11,12 +12,16 @@ import java.util.Date;
 
 public class Main extends JFrame {
     public static void main(String[] args) {
-        ApplicantModel applicantModel = new ApplicantModel();
-        InputApplicantBackend inputApplicantBackend = new InputApplicantBackend(applicantModel);
+        ApplicantModel applicantModel = jsonReaderWriter.jsonToModel("", ApplicantModel.class);
+        InputApplicantBackend inputApplicantBackend = new InputApplicantBackend(new Pointer<>(applicantModel));
         inputApplicantBackend.addApplicant("John", new Date(), 25, "Singapore", "Bisexual", "T000000000000", null, "I like kids", new ArrayList<>());
+        inputApplicantBackend.addApplicant("Nigger", new Date(), 18, "Singapore", "Dog", "Execution", null, "Hi", new ArrayList<>());
+        
         inputApplicantBackend.commitAndPushAllApplicants();
         System.out.println(applicantModel);
         String json = jsonReaderWriter.modelToJson(applicantModel);
+        StringBuilder jsonFormater = new StringBuilder(json);
+        
         System.out.println(json);
     }
 }
