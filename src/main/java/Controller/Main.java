919505adef751b3b9d680fcdb577879a2582fc3e
josep <joseph_chiu@outlook.com>
2024-06-01 00:28:38 +0800

    new testing demo snippet

--- src/main/java/Controller/Main.java
+++ src/main/java/Controller/Main.java
@@ -1,27 +1,34 @@
 package Controller;
 
 import Data.ApplicantModel;
-import Subsystems.Pointer;
-import Subsystems.jsonReaderWriter;
+import Subsystems.JsonReaderWriter;
+import Subsystems.PasswordHasher;
 
 import javax.swing.*;
 
-import java.util.ArrayList;
+
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Date;
 
 
 public class Main extends JFrame {
     public static void main(String[] args) {
-        ApplicantModel applicantModel = jsonReaderWriter.jsonToModel("", ApplicantModel.class);
-        InputApplicantBackend inputApplicantBackend = new InputApplicantBackend(new Pointer<>(applicantModel));
-        inputApplicantBackend.addApplicant("John", new Date(), 25, "Singapore", "Bisexual", "T000000000000", null, "I like kids", new ArrayList<>());
-        inputApplicantBackend.addApplicant("Nigger", new Date(), 18, "Singapore", "Dog", "Execution", null, "Hi", new ArrayList<>());
-        
-        inputApplicantBackend.commitAndPushAllApplicants();
-        System.out.println(applicantModel);
-        String json = jsonReaderWriter.modelToJson(applicantModel);
-        StringBuilder jsonFormater = new StringBuilder(json);
-        
-        System.out.println(json);
+        ApplicantModel.Applicant littleJohn = new ApplicantModel.Applicant();
+        littleJohn.setDetails("Little John",
+                new Date().getTime(),
+                20, "Bangladeshi", "Male", "123456789", "E:\\IDEAProjects\\OOP_Project\\src\\main\\java\\WhatsApp Image 2024-05-31 at 23.27.42_a7d7c681.jpg",
+                "littleJohn@student.tp.edu.sg");
+        littleJohn.setResume("I love eating galvanised steel bars");
+        String json = JsonReaderWriter.modelToJson(littleJohn);
+        try {
+            try (FileWriter fileWriter = new FileWriter(System.getProperty("user.dir") + "\\" + new PasswordHasher(String.valueOf(littleJohn.getId())).getEncodedStr() + ".json")) {
+                fileWriter.write(json);
+            }
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
+
