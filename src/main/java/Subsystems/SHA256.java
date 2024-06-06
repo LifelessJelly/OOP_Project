d30a95c0faa1946d37f7c93d87709da0de389080
josep <joseph_chiu@outlook.com>
2024-06-06 12:45:00 +0800

    abstracts the hasher

---
+++ src/main/java/Subsystems/SHA256.java
@@ -0,0 +1,10 @@
+package Subsystems;
+
+public class SHA256 {
+    public static Hasher getHasherByte(){
+        return new Hasher(false);
+    }
+    public static Hasher getHasherHex(){
+        return new Hasher(true);
+    }
+}
