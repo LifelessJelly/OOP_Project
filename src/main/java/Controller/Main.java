0f27e1e60f99e0e7c9248fadb1c2642e4a60b474
josep <joseph_chiu@outlook.com>
2024-06-06 12:46:11 +0800

    new example code added

--- src/main/java/Controller/Main.java
+++ src/main/java/Controller/Main.java
@@ -1,17 +1,18 @@
 package Controller;
 
 import Data.ApplicantModel;
-import GUI.MainApplication;
 import GUI.PersonalInfoBox;
 import Subsystems.JsonReaderWriter;
-import Subsystems.PasswordHasher;
+import Subsystems.SHA256;
 
 import javax.swing.*;
 
 
 import java.awt.*;
-import java.io.FileWriter;
+import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
 import java.util.Date;
 
 
@@ -38,9 +39,25 @@ public class Main extends JFrame {
                 20, "Bangladeshi", "Male", "123456789", "E:\\IDEAProjects\\OOP_Project\\src\\main\\java\\WhatsApp Image 2024-05-31 at 23.27.42_a7d7c681.jpg",
                 "littleJohn@student.tp.edu.sg");
         littleJohn.setResume("I love eating galvanised steel bars");
-        String json = JsonReaderWriter.modelToJson(littleJohn);
+        //Passes through the encryptor
+        ApplicantIO.writeApplicant(littleJohn, System.getProperty("user.dir"));
+        //Decrypts the file
+        ApplicantModel.Applicant johnCopy = ApplicantIO.readApplicant(new File("E:\\IDEAProjects\\OOP_Project\\32f1898a5caefac84e5ae11cbfe9ab3d2fecdb420eb69e3d2e6b7ca46fd41c1a.oop"));
+        //Will fail because the file is in a different directory (tampered with)
+        try {
+            Files.copy(new File("E:\\IDEAProjects\\OOP_Project\\32f1898a5caefac84e5ae11cbfe9ab3d2fecdb420eb69e3d2e6b7ca46fd41c1a.oop").toPath(), new File("E:\\IDEAProjects\\OOP_Project\\src\\dummyDirectory").toPath(), StandardCopyOption.REPLACE_EXISTING);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        try {
+            var willFail =  ApplicantIO.readApplicant(new File("E:\\IDEAProjects\\OOP_Project\\src\\dummyDirectory\\32f1898a5caefac84e5ae11cbfe9ab3d2fecdb420eb69e3d2e6b7ca46fd41c1a.oop"));
+        }
+        catch (Exception e) {
+            System.out.println("lol won't decrypt this shit for u");
+        }
 
-        new Main(littleJohn);
+
+        new Main(johnCopy);
 
     }
 }
