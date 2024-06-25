b9c3176d98e7f584e5fcb3c966b50a561ce32497
josep <joseph_chiu@Outlook.com>
2024-06-25 22:57:02 +0800

    Added animation cycle

--- src/main/java/GUI/Registration/SkillsInfo.java
+++ src/main/java/GUI/Registration/SkillsInfo.java
@@ -3,6 +3,9 @@ package GUI.Registration;
 import javax.swing.*;
 import java.awt.*;
 
+import static java.lang.Math.pow;
+import static java.lang.Math.round;
+
 public class SkillsInfo extends SlidingPanel{
     Mainframe mainframe;
     JLabel enterSkillsLabel;
@@ -13,7 +16,14 @@ public class SkillsInfo extends SlidingPanel{
     JPanel buttonsPanel;
     GridBagLayout layout;
     Insets movingInsets;
-
+    private int loopCycles;
+    private GridBagConstraints enterSKillsConstraints;
+    private GridBagConstraints skillsSubsConstraints;
+    private GridBagConstraints skillsScrollConstraints;
+    private GridBagConstraints skillsFieldConstraints;
+    private GridBagConstraints addRemovePanelConstraints;
+    private GridBagConstraints buttonsPanelConstraints;
+    private JPanel addRemovePanel;
 
 
     public SkillsInfo(Mainframe mainframe){
@@ -26,19 +36,22 @@ public class SkillsInfo extends SlidingPanel{
         movingInsets = new Insets(0, 0, 5, 0);
         layout = new GridBagLayout();
 
-        GridBagConstraints enterSKillsConstraints = new GridBagConstraints(0, 0, 1, 1, 0.0, 0.0,
+        enterSKillsConstraints = new GridBagConstraints(0, 0, 1, 1, 0.0, 0.0,
                 GridBagConstraints.CENTER, GridBagConstraints.BOTH,
                 movingInsets, 0, 0);
-        GridBagConstraints skillsSubsConstraints = new GridBagConstraints(0, 1, 1, 1, 0.0, 0.0,
+        skillsSubsConstraints = new GridBagConstraints(0, 1, 1, 1, 0.0, 0.0,
                 GridBagConstraints.CENTER, GridBagConstraints.BOTH,
                 movingInsets, 0, 0);
-        GridBagConstraints skillsScrollConstraints = new GridBagConstraints(0, 2, 1, 1, 0.0, 0.0,
+        skillsScrollConstraints = new GridBagConstraints(0, 2, 1, 1, 0.0, 0.0,
                 GridBagConstraints.CENTER, GridBagConstraints.BOTH,
                 movingInsets, 0, 0);
-        GridBagConstraints skillsFieldConstraints = new GridBagConstraints(0, 3, 1, 1, 0.0, 0.0,
+        skillsFieldConstraints = new GridBagConstraints(0, 3, 1, 1, 0.0, 0.0,
                 GridBagConstraints.CENTER, GridBagConstraints.BOTH,
                 movingInsets, 0, 0);
-        GridBagConstraints buttonsPanelConstraints = new GridBagConstraints(0, 4, 1, 1, 0.0, 0.0,
+        buttonsPanelConstraints = new GridBagConstraints(0, 4, 1, 1, 0.0, 0.0,
+                GridBagConstraints.CENTER, GridBagConstraints.BOTH,
+                movingInsets, 0, 0);
+        addRemovePanelConstraints = new GridBagConstraints(0, 5, 1, 1, 0.0, 0.0,
                 GridBagConstraints.CENTER, GridBagConstraints.BOTH,
                 movingInsets, 0, 0);
 
@@ -67,6 +80,15 @@ public class SkillsInfo extends SlidingPanel{
         skillsField.setEditable(true);
         this.add(skillsField, skillsFieldConstraints);
 
+        addRemovePanel = new JPanel();
+        addRemovePanel.setLayout(new GridBagLayout());
+        JButton addButton = new JButton("Add");
+        addRemovePanel.add(addButton);
+
+        JButton removeButton = new JButton("Remove");
+        addRemovePanel.add(removeButton);
+        this.add(addRemovePanel, addRemovePanelConstraints);
+
         buttonsPanel = new JPanel();
         buttonsPanel.setLayout(new GridBagLayout());
         JButton backButton = new JButton("Back");
@@ -91,5 +113,62 @@ public class SkillsInfo extends SlidingPanel{
                 new Insets(0, 0, 0, 0), 0, 0);
         buttonsPanel.add(nextButton, nextButtonConstraints);
         this.add(buttonsPanel, buttonsPanelConstraints);
+    }
+
+
+
+    @Override
+    public void slideInLeft(){
+        movingInsets.right = getStartPos();
+        this.setVisible(true);
+        updateAnimation();
+        loopCycles = 0;
+        new Timer(10, e -> {
+            if (loopCycles < 40){
+                movingInsets.right = (int) round(pow(movingInsets.right, (1/1.05))-1.5);
+                if (movingInsets.right < 0){
+                    movingInsets.right = 0;
+                }
+                updateAnimation();
+                ++loopCycles;
+            }
+            else {
+                ((Timer)e.getSource()).stop();
+            }
+        }).start();
+    }
+
+    @Override
+    public void slideInRight() {
+
+    }
+
+    @Override
+    public void slideOutLeft() {
+
+    }
+
+    @Override
+    public void slideOutRight() {
+
+    }
+
+    private void updateAnimation(){
+        layout.setConstraints(enterSkillsLabel, enterSKillsConstraints);
+        layout.setConstraints(skillsSubLabel, skillsSubsConstraints);
+        layout.setConstraints(skillsScrollPane, skillsScrollConstraints);
+        layout.setConstraints(skillsField, skillsFieldConstraints);
+        layout.setConstraints(addRemovePanel, addRemovePanelConstraints);
+        layout.setConstraints(buttonsPanel, buttonsPanelConstraints);
+        this.revalidate();
+        this.repaint();
+    }
+
+    private int getStartPos(){
+        int startPos = 0;
+        for (int i = 0; i < 40; ++i ){
+            startPos = (int) pow(startPos + 1.5, 1.05);
+        }
+        return startPos;
     }
 }
