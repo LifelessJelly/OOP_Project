caf8ade7d32cc526ffb1933c25f4d16768cc53a1
josep <joseph_chiu@Outlook.com>
2024-06-30 15:54:05 +0800

    Removed unused class

--- src/main/java/Controller/EmailSender.java
+++ src/main/java/Controller/EmailSender.java
@@ -5,7 +5,6 @@ import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 import java.util.Date;
 import java.util.Properties;
-import java.util.Scanner;
 
 public class EmailSender {
 
