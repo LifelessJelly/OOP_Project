c1e483f5246d550b17bd667620022a35d42906ba
josep <joseph_chiu@Outlook.com>
2024-06-30 16:00:25 +0800

    New implementation of the skills page (the previous one sucked

--- src/main/java/GUI/Registration/SkillsInfo.java
+++ src/main/java/GUI/Registration/SkillsInfo.java
@@ -2,29 +2,36 @@ package GUI.Registration;
 
 import javax.swing.*;
 import java.awt.*;
-
-import static java.lang.Math.pow;
-import static java.lang.Math.round;
+import java.util.ArrayList;
 
 public class SkillsInfo extends SlidingPanel{
-    Mainframe mainframe;
-    JLabel enterSkillsLabel;
-    JLabel skillsSubLabel;
-    JScrollPane skillsScrollPane;
-    JList<String> skillsList;
+    JLabel tickSkillsetsLabel;
+    JCheckBox programmingCheckBox;
+    JCheckBox industrialCheckBox;
+    JCheckBox artisticCheckBox;
+    JCheckBox communicationCheckBox;
+    JCheckBox otherCheckBox;
+    JScrollPane otherSkillsetsScrollPane;
+    JList<String> otherSkillsetsList;
+    JPanel addRemoveButtonsPanel;
+    JButton addButton;
+    JButton removeButton;
     JTextField skillsField;
-    JPanel buttonsPanel;
+    JPanel nextBackButtonsPanel;
+    JButton nextButton;
+    JPanel blankSpacer;
+    JButton backButton;
+    GridBagConstraints tickSkillsetsConstraints;
+    GridBagConstraints programmingPanelConstraints;
+    GridBagConstraints industrialPanelConstraints;
+    GridBagConstraints artisticPanelConstraints;
+    GridBagConstraints communicationPanelConstraints;
+    GridBagConstraints otherPanelConstraints;
+    GridBagConstraints otherSkillsetsScrollConstraints;
+    GridBagConstraints addRemoveButtonConstraints;
+    GridBagConstraints skillsFieldConstraints;
+    GridBagConstraints nextBackButtonConstraints;
     GridBagLayout layout;
-    Insets movingInsets;
-    private int loopCycles;
-    private GridBagConstraints enterSKillsConstraints;
-    private GridBagConstraints skillsSubsConstraints;
-    private GridBagConstraints skillsScrollConstraints;
-    private GridBagConstraints skillsFieldConstraints;
-    private GridBagConstraints addRemovePanelConstraints;
-    private GridBagConstraints buttonsPanelConstraints;
-    private JPanel addRemovePanel;
-
 
     public SkillsInfo(Mainframe mainframe){
         this.mainframe = mainframe;
@@ -33,143 +40,189 @@ public class SkillsInfo extends SlidingPanel{
 
     private void initComponents() {
 
-        movingInsets = new Insets(0, 0, 5, 0);
+        this.movingInsets = new Insets(0, 0, 5, 0);
         layout = new GridBagLayout();
-
-        enterSKillsConstraints = new GridBagConstraints(0, 0, 1, 1, 0.0, 0.0,
-                GridBagConstraints.CENTER, GridBagConstraints.BOTH,
-                movingInsets, 0, 0);
-        skillsSubsConstraints = new GridBagConstraints(0, 1, 1, 1, 0.0, 0.0,
-                GridBagConstraints.CENTER, GridBagConstraints.BOTH,
-                movingInsets, 0, 0);
-        skillsScrollConstraints = new GridBagConstraints(0, 2, 1, 1, 0.0, 0.0,
-                GridBagConstraints.CENTER, GridBagConstraints.BOTH,
-                movingInsets, 0, 0);
-        skillsFieldConstraints = new GridBagConstraints(0, 3, 1, 1, 0.0, 0.0,
-                GridBagConstraints.CENTER, GridBagConstraints.BOTH,
-                movingInsets, 0, 0);
-        buttonsPanelConstraints = new GridBagConstraints(0, 4, 1, 1, 0.0, 0.0,
-                GridBagConstraints.CENTER, GridBagConstraints.BOTH,
-                movingInsets, 0, 0);
-        addRemovePanelConstraints = new GridBagConstraints(0, 5, 1, 1, 0.0, 0.0,
-                GridBagConstraints.CENTER, GridBagConstraints.BOTH,
-                movingInsets, 0, 0);
-
-
         this.setLayout(layout);
 
-        enterSkillsLabel = new JLabel("Tell us about your skills");
-        enterSkillsLabel.setHorizontalAlignment(SwingConstants.LEFT);
-        enterSkillsLabel.setFont(enterSkillsLabel.getFont().deriveFont(22f));
-        this.add(enterSkillsLabel, enterSKillsConstraints);
+        tickSkillsetsConstraints = new GridBagConstraints(0, 0, 1, 1, 0, 0,
+                GridBagConstraints.CENTER, GridBagConstraints.BOTH,
+                movingInsets, 0, 0);
+        programmingPanelConstraints = new GridBagConstraints(0, 1, 1, 1, 0, 0,
+                GridBagConstraints.CENTER, GridBagConstraints.BOTH,
+                movingInsets, 0, 0);
+        industrialPanelConstraints = new GridBagConstraints(0, 2, 1, 1, 0, 0,
+                GridBagConstraints.CENTER, GridBagConstraints.BOTH,
+                movingInsets, 0, 0);
+        artisticPanelConstraints = new GridBagConstraints(0, 3, 1, 1, 0, 0,
+                GridBagConstraints.CENTER, GridBagConstraints.BOTH,
+                movingInsets, 0, 0);
+        communicationPanelConstraints = new GridBagConstraints(0, 4, 1, 1, 0, 0,
+                GridBagConstraints.CENTER, GridBagConstraints.BOTH,
+                movingInsets, 0, 0);
+        otherPanelConstraints = new GridBagConstraints(0, 5, 1, 1, 0, 0,
+                GridBagConstraints.CENTER, GridBagConstraints.BOTH,
+                movingInsets, 0, 0);
+        otherSkillsetsScrollConstraints = new GridBagConstraints(0, 6, 1, 1, 0, 0,
+                GridBagConstraints.CENTER, GridBagConstraints.BOTH,
+                movingInsets, 0, 0);
+        addRemoveButtonConstraints = new GridBagConstraints(0, 7, 1, 1, 0, 0,
+                GridBagConstraints.CENTER, GridBagConstraints.BOTH,
+                movingInsets, 0, 0);
+        skillsFieldConstraints = new GridBagConstraints(0, 8, 1, 1, 0, 0,
+                GridBagConstraints.CENTER, GridBagConstraints.BOTH,
+                movingInsets, 0, 0);
+        nextBackButtonConstraints = new GridBagConstraints(0, 9, 1, 1, 0, 0,
+                GridBagConstraints.CENTER, GridBagConstraints.BOTH,
+                movingInsets, 0, 0);
 
-        skillsSubLabel = new JLabel("Please separately type in each of your skills");
-        skillsSubLabel.setHorizontalAlignment(SwingConstants.LEFT);
-        skillsSubLabel.setFont(skillsSubLabel.getFont().deriveFont(18f));
-        this.add(skillsSubLabel, skillsSubsConstraints);
 
-        skillsScrollPane = new JScrollPane();
-        skillsList = new JList<>();
-        skillsList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
-        skillsList.setFont(skillsList.getFont().deriveFont(18f));
-        skillsScrollPane.setViewportView(skillsList);
-        this.add(skillsScrollPane, skillsScrollConstraints);
+
+        tickSkillsetsLabel = new JLabel("Please select the following skillsets you have:");
+        tickSkillsetsLabel.setFont(tickSkillsetsLabel.getFont().deriveFont(22f));
+        tickSkillsetsLabel.setHorizontalAlignment(SwingConstants.LEFT);
+        this.add(tickSkillsetsLabel, tickSkillsetsConstraints);
+
+        programmingCheckBox = new JCheckBox("Programming");
+        programmingCheckBox.setFont(programmingCheckBox.getFont().deriveFont(20f));
+        this.add(programmingCheckBox, programmingPanelConstraints);
+
+        industrialCheckBox = new JCheckBox("Industrial");
+        industrialCheckBox.setFont(industrialCheckBox.getFont().deriveFont(20f));
+        this.add(industrialCheckBox, industrialPanelConstraints);
+
+        artisticCheckBox = new JCheckBox("Artistic");
+        artisticCheckBox.setFont(artisticCheckBox.getFont().deriveFont(20f));
+        this.add(artisticCheckBox, artisticPanelConstraints);
+
+        communicationCheckBox = new JCheckBox("Communication");
+        communicationCheckBox.setFont(communicationCheckBox.getFont().deriveFont(20f));
+        this.add(communicationCheckBox, communicationPanelConstraints);
+
+        otherCheckBox = new JCheckBox("Other");
+        otherCheckBox.setFont(otherCheckBox.getFont().deriveFont(20f));
+        this.add(otherCheckBox, otherPanelConstraints);
+
+        otherSkillsetsScrollPane = new JScrollPane();
+        otherSkillsetsList = new JList<>();
+        otherSkillsetsList.setFont(otherSkillsetsList.getFont().deriveFont(20f));
+        otherSkillsetsList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+        otherSkillsetsScrollPane.setViewportView(otherSkillsetsList);
+        otherSkillsetsScrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED);
+        otherSkillsetsScrollPane.setVisible(false);
+        this.add(otherSkillsetsScrollPane, otherSkillsetsScrollConstraints);
+
+        addRemoveButtonsPanel = new JPanel();
+        addRemoveButtonsPanel.setLayout(new GridBagLayout());
+        addButton = new JButton("Add Skill");
+        GridBagConstraints addButtonConstraints = new GridBagConstraints(0, 0, 1, 1, 1, 0,
+                GridBagConstraints.CENTER, GridBagConstraints.BOTH,
+                new Insets(0, 0, 0, 0), 0, 0);
+        addButton.setFont(addButton.getFont().deriveFont(20f));
+        addRemoveButtonsPanel.add(addButton, addButtonConstraints);
+
+        removeButton = new JButton("Remove Skill");
+        GridBagConstraints removeButtonConstraints = new GridBagConstraints(1, 0, 1, 1, 1, 0,
+                GridBagConstraints.CENTER, GridBagConstraints.BOTH,
+                new Insets(0, 0, 0, 0), 0, 0);
+        removeButton.setFont(removeButton.getFont().deriveFont(20f));
+        addRemoveButtonsPanel.add(removeButton, removeButtonConstraints);
+        addRemoveButtonsPanel.setVisible(false);
+        this.add(addRemoveButtonsPanel, addRemoveButtonConstraints);
 
         skillsField = new JTextField();
-        skillsField.setFont(skillsField.getFont().deriveFont(18f));
-        skillsField.setEditable(true);
+        skillsField.setFont(skillsField.getFont().deriveFont(20f));
+        skillsField.setVisible(false);
         this.add(skillsField, skillsFieldConstraints);
 
-        addRemovePanel = new JPanel();
-        addRemovePanel.setLayout(new GridBagLayout());
-        JButton addButton = new JButton("Add");
-        addRemovePanel.add(addButton);
-
-        JButton removeButton = new JButton("Remove");
-        addRemovePanel.add(removeButton);
-        this.add(addRemovePanel, addRemovePanelConstraints);
-
-        buttonsPanel = new JPanel();
-        buttonsPanel.setLayout(new GridBagLayout());
-        JButton backButton = new JButton("Back");
-        backButton.setHorizontalAlignment(SwingConstants.CENTER);
-        backButton.setFont(backButton.getFont().deriveFont(18f));
-        GridBagConstraints backButtonConstraints = new GridBagConstraints(0, 0, 1, 1, 0.0, 0.0,
+        nextBackButtonsPanel = new JPanel();
+        nextBackButtonsPanel.setLayout(new GridBagLayout());
+        nextButton = new JButton("Next");
+        nextButton.setFont(nextButton.getFont().deriveFont(20f));
+        GridBagConstraints nextButtonConstraints = new GridBagConstraints(2, 0, 1, 1, 0, 0,
                 GridBagConstraints.CENTER, GridBagConstraints.BOTH,
                 new Insets(0, 0, 0, 0), 0, 0);
-        buttonsPanel.add(backButton, backButtonConstraints);
-        JPanel blankSpacerPanel = new JPanel();
-        blankSpacerPanel.setMinimumSize(new Dimension(640, 0));
-        blankSpacerPanel.setPreferredSize(new Dimension(640, 0));
-        GridBagConstraints blankSpacerConstraints = new GridBagConstraints(1, 0, 1, 1, 0.0, 0.0,
+        nextBackButtonsPanel.add(nextButton, nextButtonConstraints);
+
+        blankSpacer = new JPanel();
+        blankSpacer.setMinimumSize(new Dimension(640, 0));
+        blankSpacer.setPreferredSize(new Dimension(640, 0));
+        GridBagConstraints blankSpacerConstraints = new GridBagConstraints(1, 0, 1, 1, 0, 0,
                 GridBagConstraints.CENTER, GridBagConstraints.BOTH,
                 new Insets(0, 0, 0, 0), 0, 0);
-        buttonsPanel.add(blankSpacerPanel, blankSpacerConstraints);
-        JButton nextButton = new JButton("Next");
-        nextButton.setHorizontalAlignment(SwingConstants.CENTER);
-        nextButton.setFont(backButton.getFont().deriveFont(18f));
-        GridBagConstraints nextButtonConstraints = new GridBagConstraints(2, 0, 1, 1, 0.0, 0.0,
+        nextBackButtonsPanel.add(blankSpacer, blankSpacerConstraints);
+
+        backButton = new JButton("Back");
+        backButton.setFont(backButton.getFont().deriveFont(20f));
+        GridBagConstraints backButtonConstraints = new GridBagConstraints(0, 0, 1, 1, 0, 0,
                 GridBagConstraints.CENTER, GridBagConstraints.BOTH,
                 new Insets(0, 0, 0, 0), 0, 0);
-        buttonsPanel.add(nextButton, nextButtonConstraints);
-        this.add(buttonsPanel, buttonsPanelConstraints);
-    }
+        nextBackButtonsPanel.add(backButton, backButtonConstraints);
+
+        this.add(nextBackButtonsPanel, nextBackButtonConstraints);
 
 
+        otherCheckBox.addActionListener(e -> {
+            otherSkillsetsScrollPane.setVisible(otherCheckBox.isSelected());
+            addRemoveButtonsPanel.setVisible(otherCheckBox.isSelected());
+            skillsField.setVisible(otherCheckBox.isSelected());
+            updateAnimation();
+        });
 
-    @Override
-    public void slideInLeft(){
-        movingInsets.right = getStartPos();
-        this.setVisible(true);
-        updateAnimation();
-        loopCycles = 0;
-        new Timer(10, e -> {
-            if (loopCycles < 40){
-                movingInsets.right = (int) round(pow(movingInsets.right, (1/1.05))-1.5);
-                if (movingInsets.right < 0){
-                    movingInsets.right = 0;
+        addButton.addActionListener(e -> {
+            mainframe.getController().addSkill(skillsField.getText());
+            otherSkillsetsList.setModel(getSkillsListModel());
+            skillsField.setText("");
+        });
+        removeButton.addActionListener(e -> {
+            if (otherSkillsetsList.getSelectedIndex() == -1) {
+                //return early
+                return;
+            }
+            mainframe.getController().removeSkill(otherSkillsetsList.getSelectedIndex());
+            otherSkillsetsList.setModel(getSkillsListModel());
+        });
+        backButton.addActionListener(e -> {
+            mainframe.panelOutroLeft();
+        });
+        nextButton.addActionListener(e -> {
+            ArrayList<String> defaultSkillsArrList = new ArrayList<>();
+            for (Component component : this.getComponents()) {
+                if (component instanceof JCheckBox) {
+                    if (((JCheckBox) component).isSelected()) {
+                        defaultSkillsArrList.add(((JCheckBox) component).getText());
+                    }
                 }
-                updateAnimation();
-                ++loopCycles;
             }
-            else {
-                ((Timer)e.getSource()).stop();
-            }
-        }).start();
-    }
-
-    @Override
-    public void slideInRight() {
+            mainframe.getController().registerDefaultSkills(defaultSkillsArrList.toArray(new String[0]));
+            mainframe.getController().registerUserDefinedSkills(mainframe.getController().getSkills());
+            mainframe.panelOutroRight();
+        });
 
     }
 
-    @Override
-    public void slideOutLeft() {
-
+    private DefaultListModel<String> getSkillsListModel() {
+        String[] applicantSkills = mainframe.getController().getSkills();
+        DefaultListModel<String> skillsListModel = new DefaultListModel<>();
+        for (String applicantSkill : applicantSkills) {
+            skillsListModel.addElement(applicantSkill);
+        }
+        return skillsListModel;
     }
 
-    @Override
-    public void slideOutRight() {
-
-    }
 
     @Override
     protected void updateAnimation(){
-        layout.setConstraints(enterSkillsLabel, enterSKillsConstraints);
-        layout.setConstraints(skillsSubLabel, skillsSubsConstraints);
-        layout.setConstraints(skillsScrollPane, skillsScrollConstraints);
+        layout.setConstraints(tickSkillsetsLabel, tickSkillsetsConstraints);
+        layout.setConstraints(programmingCheckBox, programmingPanelConstraints);
+        layout.setConstraints(industrialCheckBox, industrialPanelConstraints);
+        layout.setConstraints(artisticCheckBox, artisticPanelConstraints);
+        layout.setConstraints(communicationCheckBox, communicationPanelConstraints);
+        layout.setConstraints(otherCheckBox, otherPanelConstraints);
+        layout.setConstraints(otherSkillsetsScrollPane, otherSkillsetsScrollConstraints);
+        layout.setConstraints(addRemoveButtonsPanel, addRemoveButtonConstraints);
         layout.setConstraints(skillsField, skillsFieldConstraints);
-        layout.setConstraints(addRemovePanel, addRemovePanelConstraints);
-        layout.setConstraints(buttonsPanel, buttonsPanelConstraints);
+        layout.setConstraints(nextBackButtonsPanel, nextBackButtonConstraints);
         this.revalidate();
         this.repaint();
-    }
-
-    private int getStartPos(){
-        int startPos = 0;
-        for (int i = 0; i < 40; ++i ){
-            startPos = (int) pow(startPos + 1.5, 1.05);
-        }
-        return startPos;
     }
 }
