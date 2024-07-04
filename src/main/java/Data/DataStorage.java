4700d99cd6ece80874fd0b6b788590b04a80557b
onath_li <onathan.v.2@gmail.com>
2024-07-04 17:20:38 +0800

    Commented on DataStorage.java

--- src/main/java/Data/DataStorage.java
+++ src/main/java/Data/DataStorage.java
@@ -11,35 +11,44 @@ public class DataStorage {
         experience = new ArrayList<>();
     }
 
+    //skill getter
     public String[] getSkills() {
         return skills.toArray(new String[0]);
     }
 
+    //skill adder
     public void addSkill(String skill) {
         skills.add(skill);
     }
 
+    //skill remove
     public void removeSkill(int index){
         skills.remove(index);
     }
 
+    //get applicant experience array
     public ApplicantExperience[] getExperience() {
         return experience.toArray(new ApplicantExperience[0]);
     }
+
+    //add applicant experience array
     public void addExperience(String company, String position, int yearStart, int yearEnd) {
         experience.add(new ApplicantExperience(company, position, yearStart, yearEnd));
 
         experience.sort(new CompareJobDates());
     }
+
+    //remove the experience, pass through index
     public void removeExperience(int index){
         experience.remove(index);
     }
 
-    private static class CompareJobDates implements Comparator<ApplicantExperience> {
 
+    private static class CompareJobDates implements Comparator<ApplicantExperience> {
         @Override
         public int compare(ApplicantExperience o1, ApplicantExperience o2) {
             return o1.getYearBegin() - o2.getYearBegin();
+            //compares the experience length of the applicants (is this to be implemented in the manager console josep?)
         }
     }
 }
