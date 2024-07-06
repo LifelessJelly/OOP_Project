659ab0d11217233e9ecef43a241a77f0ec99e60d
josep <joseph_chiu@outlook.com>
2024-07-07 00:13:49 +0800

    Removed code that likely doesn't work on anyone else's computer other than mine

--- src/main/java/Controller/Main.java
+++ src/main/java/Controller/Main.java
@@ -1,6 +1,5 @@
 package Controller;
 
-import GUI.Registration.Mainframe;
 import GUI.Infobase.InfobaseMainframe;
 
 
@@ -22,8 +21,8 @@ public class Main {
             UIManager.setLookAndFeel(new FlatDarkLaf());
         } catch (UnsupportedLookAndFeelException e) {
             throw new RuntimeException(e);
+
         }
-        System.out.println(ImageBase64.imageToBase64("C:\\Users\\User\\Downloads\\Lab73_Java\\OOP_Project\\check_24dp_E8EAED_FILL0_wght400_GRAD0_opsz24(1).png"));
         new InfobaseMainframe();
     }
 }
