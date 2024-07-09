96b90aaaa9ff8697439116ab3ba57c859b442306
josep <joseph_chiu@Outlook.com>
2024-07-10 00:40:40 +0800

    Created a Java class to act as a key for the translation table

---
+++ src/main/java/Data/TranslationKey.java
@@ -0,0 +1,33 @@
+package Data;
+
+import java.util.Objects;
+
+public class TranslationKey {
+    String language;
+    String key;
+
+    public TranslationKey(String language, String key) {
+        switch (language){
+            case "en": //british english; screw american english
+            case "cn":
+                break;
+            default:
+                throw new IllegalArgumentException("Language not recognized");
+        }
+        this.language = language;
+        this.key = key;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        TranslationKey that = (TranslationKey) o;
+        return Objects.equals(language, that.language) && Objects.equals(key, that.key);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(language, key);
+    }
+}
