d82f4703689bd0b16c3915e5d1d34e8e5de1612e
josep <joseph_chiu@Outlook.com>
2024-07-13 23:51:25 +0800

    -Imported files no longer default to their original file format, all file formats are stored as .jpegs -Added an overload method to directly pass in an imported image to be converted

--- src/main/java/subsystems/ImageBase64.java
+++ src/main/java/subsystems/ImageBase64.java
@@ -1,12 +1,12 @@
 package subsystems;
 
+import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
 import java.util.Base64;
-import java.util.regex.Pattern;
 import javax.imageio.ImageIO;
 
 public class ImageBase64 {
@@ -18,34 +18,42 @@ public class ImageBase64 {
      * @throws RuntimeException if there are any issues during the conversion process
      */
     public static String imageToBase64(String pathToImage) {
-        BufferedImage image;
+        Image image;
         ImageIO.setUseCache(false);
         try {
             image = ImageIO.read(new File(pathToImage));
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-        Pattern pngPattern = Pattern.compile("png$", Pattern.CASE_INSENSITIVE);
-        Pattern jpgPattern = Pattern.compile("jpg$", Pattern.CASE_INSENSITIVE);
-        Pattern jpegPattern = Pattern.compile("jpeg$", Pattern.CASE_INSENSITIVE);
-        String fileType = "";
-        if (pngPattern.matcher(pathToImage).find()) {
-            fileType = "png";
-        }
-        if (jpgPattern.matcher(pathToImage).find()) {
-            fileType = "jpg";
-        }
-        if (jpegPattern.matcher(pathToImage).find()) {
-            fileType = "jpeg";
-        }
+//        Pattern pngPattern = Pattern.compile("png$", Pattern.CASE_INSENSITIVE);
+//        Pattern jpgPattern = Pattern.compile("jpg$", Pattern.CASE_INSENSITIVE);
+//        Pattern jpegPattern = Pattern.compile("jpeg$", Pattern.CASE_INSENSITIVE);
+//        String fileType = "";
+//        if (pngPattern.matcher(pathToImage).find()) {
+//            fileType = "png";
+//        }
+//        if (jpgPattern.matcher(pathToImage).find()) {
+//            fileType = "jpg";
+//        }
+//        if (jpegPattern.matcher(pathToImage).find()) {
+//            fileType = "jpeg";
+//        }
+        return imageToBase64(image);
+
+    }
+
+    public static String imageToBase64(Image image){
+        BufferedImage bufferedImage = new BufferedImage(image.getWidth(null), image.getHeight(null), BufferedImage.TYPE_INT_ARGB);
+        bufferedImage.getGraphics().drawImage(image, 0, 0, null);
         ByteArrayOutputStream baos = new ByteArrayOutputStream();
         try {
-            ImageIO.write(image, fileType, baos);                   //writing image data to baos
+            ImageIO.write(bufferedImage, "jpeg", baos); //smaller file size to load faster
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
         byte[] bytes = baos.toByteArray();                          //extract data as bytearray
         return Base64.getEncoder().encodeToString(bytes);
+
     }
 
     /**
