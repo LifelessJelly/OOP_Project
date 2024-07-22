615381c999f868e35df4bcc8c34db9c2c6e8819e
josep <joseph_chiu@outlook.com>
2024-07-23 01:12:30 +0800

    deleted read and write applicant functions with encryptors

--- src/main/java/controller/DataIO.java
+++ src/main/java/controller/DataIO.java
@@ -1,13 +1,6 @@
 package controller;
 
 
-import data.Applicant;
-import subsystems.FileContentEncryptor;
-import subsystems.JsonReaderWriter;
-import subsystems.SHA256;
-
-import java.io.File;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
@@ -18,32 +11,6 @@ import java.nio.file.Paths;
 //store every applicant's data discretely
 public class DataIO {
 
-    //still broken don't use
-    public static Applicant readApplicant(File pathToApplicantsFile) {
-        String fileDir = pathToApplicantsFile.getPath();
-        String parentDir = pathToApplicantsFile.getParent();
-        return JsonReaderWriter.jsonToModel(FileContentEncryptor.decrypt(readFile(fileDir), parentDir), Applicant.class);
-
-    }
-
-    /**
-     * Writes the applicant information to a file in the specified directory after encrypting it.
-     *
-     * @param applicant The Applicant object containing the information to be written.
-     * @param pathToDirectory The path to the directory where the file will be written.
-     */
-    public static void writeApplicant(Applicant applicant, String pathToDirectory) {
-        try {
-            try (FileWriter newFile = new FileWriter(pathToDirectory + "\\" + new String(SHA256.getHasherHex().hashString(String.valueOf(System.currentTimeMillis()))) + ".oop")) {
-                newFile.write(FileContentEncryptor.encrypt(JsonReaderWriter.modelToJson(applicant), pathToDirectory));
-            }
-        }
-        catch (IOException e) {
-            e.printStackTrace(System.out);
-        }
-    }
-
-
     public static String readFile(String pathToFile) {
         try {
             return new String(Files.readAllBytes(Paths.get(pathToFile)));
@@ -51,5 +18,7 @@ public class DataIO {
             throw new RuntimeException(e);
         }
     }
+
+    //TODO add write method
 
 }
