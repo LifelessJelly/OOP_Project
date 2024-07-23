feb1610b027cd40d619ab37ad88695365062e00c
josep <joseph_chiu@Outlook.com>
2024-07-23 23:16:19 +0800

    Changed main

--- src/main/java/controller/Main.java
+++ src/main/java/controller/Main.java
@@ -2,7 +2,13 @@ package controller;
 
 
 import com.formdev.flatlaf.FlatLightLaf;
+import subsystems.ImageBase64;
+
+import javax.imageio.ImageIO;
 import javax.swing.*;
+import java.awt.*;
+import java.io.File;
+import java.io.IOException;
 
 
 public class Main {
@@ -12,35 +18,29 @@ public class Main {
     public static void main(String[] args) {
 
 
-        try {
-            UIManager.setLookAndFeel(new FlatLightLaf());
-        } catch (UnsupportedLookAndFeelException e) {
-            throw new RuntimeException(e);
-
-        }
         new LoginMainframe();
-
+//
 //        Image image1;
-//        Image image2;
-//        Image image3;
-//        Image image4;
-//        Image image5;
-//        Image image6;
-//        Image image7;
-//        Image image8;
+////        Image image2;
+////        Image image3;
+////        Image image4;
+////        Image image5;
+////        Image image6;
+////        Image image7;
+////        Image image8;
 //        try {
-//            image1 = ImageIO.read(new File("E:\\IDEAProjects\\OOP\\CompanyLogoThemes\\CompanyLogoBorealis.png"));
-//            image2 = ImageIO.read(new File("E:\\IDEAProjects\\OOP\\CompanyLogoThemes\\CompanyLogoPornhub.png"));
-//            image3 = ImageIO.read(new File("E:\\IDEAProjects\\OOP\\CompanyLogoThemes\\CompanyLogoNostalgia.png"));
-//            image4 = ImageIO.read(new File("E:\\IDEAProjects\\OOP\\CompanyLogoThemes\\CompanyLogoNight.png"));
-//            image5 = ImageIO.read(new File("E:\\IDEAProjects\\OOP\\CompanyLogoThemes\\CompanyLogoMint.png"));
-//            image6 = ImageIO.read(new File("E:\\IDEAProjects\\OOP\\CompanyLogoThemes\\CompanyLogoTide.png"));
-//            image7 = ImageIO.read(new File("E:\\IDEAProjects\\OOP\\CompanyLogoThemes\\CompanyLogoUnicorn.png"));
-//            image8 = ImageIO.read(new File("E:\\IDEAProjects\\OOP\\AddToShortlistIcon.png"));
+//            image1 = ImageIO.read(new File("C:\\Users\\User\\IdeaProjects\\OOP_Project_real\\ProfileIcon.png"));
+////            image2 = ImageIO.read(new File("E:\\IDEAProjects\\OOP\\CompanyLogoThemes\\CompanyLogoPornhub.png"));
+////            image3 = ImageIO.read(new File("E:\\IDEAProjects\\OOP\\CompanyLogoThemes\\CompanyLogoNostalgia.png"));
+////            image4 = ImageIO.read(new File("E:\\IDEAProjects\\OOP\\CompanyLogoThemes\\CompanyLogoNight.png"));
+////            image5 = ImageIO.read(new File("E:\\IDEAProjects\\OOP\\CompanyLogoThemes\\CompanyLogoMint.png"));
+////            image6 = ImageIO.read(new File("E:\\IDEAProjects\\OOP\\CompanyLogoThemes\\CompanyLogoTide.png"));
+////            image7 = ImageIO.read(new File("E:\\IDEAProjects\\OOP\\CompanyLogoThemes\\CompanyLogoUnicorn.png"));
+////            image8 = ImageIO.read(new File("E:\\IDEAProjects\\OOP\\AddToShortlistIcon.png"));
 //        } catch (IOException e) {
 //            throw new RuntimeException(e);
 //        }
-//
+////
 //        System.out.println(ImageBase64.imageToBase64(image1));
 //        System.out.println(ImageBase64.imageToBase64(image2));
 //        System.out.println(ImageBase64.imageToBase64(image3));
