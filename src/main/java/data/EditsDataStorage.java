77949b8fd86249c94839a587f6c6d997ea619ec0
josep <joseph_chiu@Outlook.com>
2024-07-29 18:20:50 +0800

    stuffed every applicant attribute into this class to comply with MVC

--- src/main/java/data/EditsDataStorage.java
+++ src/main/java/data/EditsDataStorage.java
@@ -1,22 +1,37 @@
 package data;
 
+import java.awt.image.BufferedImage;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class EditsDataStorage {
 
     private String name;
-    private long birthdate;
     private int age;
     private String gender;
     private String nric;
-    private String imageBase64;
+    private BufferedImage image;
     private String email;
+    private String birthdate;
+    private final List<String> skills;
 
-    private final ArrayList<String> skills;
-
-    public EditsDataStorage() {
+    public EditsDataStorage(Applicant applicant) {
         skills = new ArrayList<>();
+        if (applicant != null) {
+            String[] skills = applicant.getSkills();
+            if (skills != null) {
+                this.skills.addAll(Arrays.asList(skills));
+            }
+            name = applicant.getName();
+            age = applicant.getAge();
+            gender = applicant.getGender();
+            nric = applicant.getNRIC();
+            image = applicant.getImage();
+            email = applicant.getEmail();
+            birthdate = applicant.getBirthdate();
+        }
+
     }
 
     //skill getter
@@ -34,13 +49,62 @@ public class EditsDataStorage {
         skills.remove(index);
     }
 
-    //get applicant experience array
-
-    public void importSkills(String[] skills) {
-        if (skills != null) {
-            this.skills.addAll(Arrays.asList(skills));
-        }
+    public String getName() {
+        return name;
     }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBirthdate() {
+        return birthdate;
+    }
+
+    public void setBirthdate(String birthdate) {
+        this.birthdate = birthdate;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getNric() {
+        return nric;
+    }
+
+    public void setNric(String nric) {
+        this.nric = nric;
+    }
+
+    public BufferedImage getImage() {
+        return image;
+    }
+
+    public void setImage(BufferedImage image) {
+        this.image = image;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
     //add applicant experience array
     //remove the experience, pass through index
 
