9f28e625e8875086d4338f7d7f88d0772fca7872
josep <joseph_chiu@Outlook.com>
2024-08-02 22:59:29 +0800

    Add methods to retrieve JComboBox with base hour values and 15-minute intervals in DateSelectorHelper
    -Hail Steven

--- src/main/java/gui/DateSelectorHelper.java
+++ src/main/java/gui/DateSelectorHelper.java
@@ -74,4 +74,12 @@ public class DateSelectorHelper {
             }
         };
     }
+
+    public static JComboBox<Integer> comboBoxGetBaseHour() {
+        return new JComboBox<>(new Integer[]{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23});
+    }
+
+    public static JComboBox<Integer> comboBoxGet15MinuteInterval() {
+        return new JComboBox<>(new Integer[]{0, 15, 30, 45});
+    }
 }
