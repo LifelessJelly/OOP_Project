f4c421c8d20c37fab8fd027318ab9610c0aa281d
josep <joseph_chiu@Outlook.com>
2024-08-02 23:00:18 +0800

    Remove redundant fields and methods related to applicant details. Refactor the skill editing method in EditsDataStorage.
    -Hail Steven

--- src/main/java/data/EditsDataStorage.java
+++ src/main/java/data/EditsDataStorage.java
@@ -1,20 +1,12 @@
 package data;
 
-import java.awt.image.BufferedImage;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class EditsDataStorage {
-
-    private String name;
-    private int age;
-    private String gender;
-    private String nric;
-    private BufferedImage image;
-    private String email;
-    private String birthdate;
     private final List<String> skills;
+
 
     public EditsDataStorage(Applicant applicant) {
         skills = new ArrayList<>();
@@ -23,15 +15,7 @@ public class EditsDataStorage {
             if (skills != null) {
                 this.skills.addAll(Arrays.asList(skills));
             }
-            name = applicant.getName();
-            age = applicant.getAge();
-            gender = applicant.getGender();
-            nric = applicant.getNRIC();
-            image = applicant.getImage();
-            email = applicant.getEmail();
-            birthdate = applicant.getBirthdate();
         }
-
     }
 
     //skill getter
@@ -49,63 +33,7 @@ public class EditsDataStorage {
         skills.remove(index);
     }
 
-    public String getName() {
-        return name;
+    public void editSkill(int index, String skill) {
+        skills.set(index, skill);
     }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getBirthdate() {
-        return birthdate;
-    }
-
-    public void setBirthdate(String birthdate) {
-        this.birthdate = birthdate;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-    public String getGender() {
-        return gender;
-    }
-
-    public void setGender(String gender) {
-        this.gender = gender;
-    }
-
-    public String getNric() {
-        return nric;
-    }
-
-    public void setNric(String nric) {
-        this.nric = nric;
-    }
-
-    public BufferedImage getImage() {
-        return image;
-    }
-
-    public void setImage(BufferedImage image) {
-        this.image = image;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    //add applicant experience array
-    //remove the experience, pass through index
-
 }
