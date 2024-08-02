069df8b6d27637ee17643e211e01d7029b3bbb30
josep <joseph_chiu@Outlook.com>
2024-08-02 23:06:38 +0800

    Update ApplicantListPage GUI:
    - Added 'PDFBase64' import in the file
    - Renamed 'waitingShortlist' checkbox to 'waitingShortlistCheckBox'
    - Added 'setJobRoleButton', 'acceptedWaitingManagerCheckBox', 'shortlistedWaitingHRCheckBox', and 'acceptApplicantButton' buttons
    - Adjusted row height in the table
    - Updated the layout weights in the filterMenu
    - Added new checkboxes for filtering and exclude skills
    - Added functionality and buttons for accepting applicants, setting job roles, and shortlisting applicants
    - Set button states based on applicant status and user authorization
    - Refactored updateModel method to handle different filter stages and exclude skills
    
    -Hail Steven

--- src/main/java/gui/infobase/ApplicantListPage.java
+++ src/main/java/gui/infobase/ApplicantListPage.java
@@ -2,6 +2,7 @@ package gui.infobase;
 
 
 import controller.InfobaseMainframe;
+import controller.PDFBase64;
 import data.Applicant;
 import data.Staff;
 import gui.ImageEmbedded;
@@ -26,7 +27,7 @@ public class ApplicantListPage extends JPanel{
     private JCheckBox shortlistedCheckBox;
     private JCheckBox acceptedCheckBox;
     private JTextField filterField;
-    private JCheckBox waitingShortlist;
+    private JCheckBox waitingShortlistCheckBox;
     private JCheckBox programmingCheckBox;
     private JCheckBox industrialCheckBox;
     private JCheckBox artisticCheckBox;
@@ -38,6 +39,10 @@ public class ApplicantListPage extends JPanel{
     private float alpha;
     private JButton setInterviewDateButton;
     private JButton shortlistApplicantButton;
+    private JButton setJobRoleButton;
+    private JCheckBox acceptedWaitingManagerCheckBox;
+    private JCheckBox shortlistedWaitingHRCheckBox;
+    private JButton acceptApplicantButton;
 
     public ApplicantListPage(InfobaseMainframe mainframe) {
         this.main = mainframe;
@@ -80,7 +85,7 @@ public class ApplicantListPage extends JPanel{
         tableScroll.setViewportBorder(BorderFactory.createEmptyBorder());
         table.setBorder(BorderFactory.createEmptyBorder());
         table.getTableHeader().setBorder(BorderFactory.createEmptyBorder());
-        table.setRowHeight(200);
+        table.setRowHeight(275);
         table.setMinimumSize(new Dimension(0, 100));
 
         table.getColumnModel().getColumn(0).setCellRenderer(new TableImageRender());
@@ -96,7 +101,7 @@ public class ApplicantListPage extends JPanel{
 
         JPanel filterMenu = new JPanel(new GridBagLayout());
         {
-            ((GridBagLayout)filterMenu.getLayout()).rowWeights = new double[]{0, 0, 0, 0, 0, 0, 0, 0, 0,  1e-4};
+            ((GridBagLayout)filterMenu.getLayout()).rowWeights = new double[]{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1e-4};
             ((GridBagLayout)filterMenu.getLayout()).columnWeights = new double[]{1e-4};
             JLabel nameFilterLabel = new JLabel(main.getLocale("ApplicantListPage.JLabel.filterName"));
             nameFilterLabel.setFont(nameFilterLabel.getFont().deriveFont(18f));
@@ -118,48 +123,69 @@ public class ApplicantListPage extends JPanel{
                     GridBagConstraints.NORTHWEST, GridBagConstraints.NONE, new Insets(0, 5, 10, 0), 0, 0);
             filterMenu.add(statusFilterLabel, statusFilterConstarints);
 
-            waitingShortlist = new JCheckBox(main.getLocale("ApplicantListPage.JCheckBox.waitingShortlist"));
-            waitingShortlist.setSelected(true);
-            waitingShortlist.setFont(waitingShortlist.getFont().deriveFont(16f));
+            waitingShortlistCheckBox = new JCheckBox(main.getLocale("ApplicantListPage.JCheckBox.waitingShortlist"));
+            waitingShortlistCheckBox.setSelected(true);
+            waitingShortlistCheckBox.setFont(waitingShortlistCheckBox.getFont().deriveFont(16f));
             GridBagConstraints waitingFilterConstraints = new GridBagConstraints(0, 3, 1, 1, 0, 0,
                     GridBagConstraints.NORTHWEST, GridBagConstraints.NONE, new Insets(0, 5, 0, 0), 0, 0);
-            filterMenu.add(waitingShortlist, waitingFilterConstraints);
+            filterMenu.add(waitingShortlistCheckBox, waitingFilterConstraints);
+
+            shortlistedWaitingHRCheckBox = new JCheckBox(main.getLocale("ApplicantListPage.JCheckBox.shortlistedWaitingHR"));
+            shortlistedWaitingHRCheckBox.setSelected(true);
+            shortlistedWaitingHRCheckBox.setFont(shortlistedWaitingHRCheckBox.getFont().deriveFont(16f));
+            GridBagConstraints shortlistedWaitingFilterConstraints = new GridBagConstraints(0, 4, 1, 1, 0, 0,
+                    GridBagConstraints.NORTHWEST, GridBagConstraints.NONE, new Insets(0, 5, 0, 0), 0, 0);
+            filterMenu.add(shortlistedWaitingHRCheckBox, shortlistedWaitingFilterConstraints);
 
             shortlistedCheckBox = new JCheckBox(main.getLocale("ApplicantListPage.JCheckBox.shortlisted"));
             shortlistedCheckBox.setSelected(true);
             shortlistedCheckBox.setFont(shortlistedCheckBox.getFont().deriveFont(16f));
-            GridBagConstraints statusFilterConstraints = new GridBagConstraints(0, 4, 1, 1, 0, 0,
+            GridBagConstraints shortlistedFilterConstraints = new GridBagConstraints(0, 5, 1, 1, 0, 0,
                     GridBagConstraints.NORTHWEST, GridBagConstraints.NONE, new Insets(0, 5, 0, 0), 0, 0);
-            filterMenu.add(shortlistedCheckBox, statusFilterConstraints);
+            filterMenu.add(shortlistedCheckBox, shortlistedFilterConstraints);
+
+            acceptedWaitingManagerCheckBox = new JCheckBox(main.getLocale("ApplicantListPage.JCheckBox.acceptedWaitingManager"));
+            acceptedWaitingManagerCheckBox.setSelected(true);
+            acceptedWaitingManagerCheckBox.setFont(acceptedWaitingManagerCheckBox.getFont().deriveFont(16f));
+            GridBagConstraints acceptedWaitingFilterConstraints = new GridBagConstraints(0, 6, 1, 1, 0, 0,
+                    GridBagConstraints.NORTHWEST, GridBagConstraints.NONE, new Insets(0, 5, 0, 0), 0, 0);
+            filterMenu.add(acceptedWaitingManagerCheckBox, acceptedWaitingFilterConstraints);
 
             acceptedCheckBox = new JCheckBox(main.getLocale("ApplicantListPage.JCheckBox.accepted"));
             acceptedCheckBox.setSelected(true);
             acceptedCheckBox.setFont(acceptedCheckBox.getFont().deriveFont(16f));
-            GridBagConstraints acceptedFilterConstraints = new GridBagConstraints(0, 5, 1, 1, 0, 0,
-                    GridBagConstraints.NORTHWEST, GridBagConstraints.NONE, new Insets(0, 5, 0, 0), 0, 0);
+            GridBagConstraints acceptedFilterConstraints = new GridBagConstraints(0, 7, 1, 1, 0, 0,
+                    GridBagConstraints.NORTHWEST, GridBagConstraints.NONE, new Insets(0, 7, 0, 0), 0, 0);
             filterMenu.add(acceptedCheckBox, acceptedFilterConstraints);
+
+            JLabel excludeSkillLabel = new JLabel(main.getLocale("ApplicantListPage.JLabel.excludeSkill"));
+            excludeSkillLabel.setFont(excludeSkillLabel.getFont().deriveFont(18f));
+            excludeSkillLabel.setHorizontalAlignment(SwingConstants.LEFT);
+            GridBagConstraints excludeSkillConstraints = new GridBagConstraints(0, 8, 1, 1, 0, 0,
+                    GridBagConstraints.NORTHWEST, GridBagConstraints.NONE, new Insets(25, 5, 0, 0), 0, 0);
+            filterMenu.add(excludeSkillLabel, excludeSkillConstraints);
 
             programmingCheckBox = new JCheckBox(main.getLocale("ShortlistPage.JCheckBox.programming"));
             programmingCheckBox.setFont(programmingCheckBox.getFont().deriveFont(18f));
-            GridBagConstraints programmingConstraints = new GridBagConstraints(0, 6, 1, 1, 0, 0,
+            GridBagConstraints programmingConstraints = new GridBagConstraints(0, 9, 1, 1, 0, 0,
                     GridBagConstraints.NORTHWEST, GridBagConstraints.NONE, new Insets(0, 5, 0, 0), 0, 0);
             filterMenu.add(programmingCheckBox, programmingConstraints);
 
             industrialCheckBox = new JCheckBox(main.getLocale("ShortlistPage.JCheckBox.industrial"));
             industrialCheckBox.setFont(industrialCheckBox.getFont().deriveFont(18f));
-            GridBagConstraints industrialConstraints = new GridBagConstraints(0, 7, 1, 1, 0, 0,
+            GridBagConstraints industrialConstraints = new GridBagConstraints(0, 10, 1, 1, 0, 0,
                     GridBagConstraints.NORTHWEST, GridBagConstraints.NONE, new Insets(0, 5, 0, 0), 0, 0);
             filterMenu.add(industrialCheckBox, industrialConstraints);
 
             artisticCheckBox = new JCheckBox(main.getLocale("ShortlistPage.JCheckBox.artistic"));
             artisticCheckBox.setFont(artisticCheckBox.getFont().deriveFont(18f));
-            GridBagConstraints artisticConstraints = new GridBagConstraints(0, 8, 1, 1, 0, 0,
+            GridBagConstraints artisticConstraints = new GridBagConstraints(0, 11, 1, 1, 0, 0,
                     GridBagConstraints.NORTHWEST, GridBagConstraints.NONE, new Insets(0, 5, 0, 0), 0, 0);
             filterMenu.add(artisticCheckBox, artisticConstraints);
 
             communicationCheckBox = new JCheckBox(main.getLocale("ShortlistPage.JCheckBox.communication"));
             communicationCheckBox.setFont(communicationCheckBox.getFont().deriveFont(18f));
-            GridBagConstraints communicationConstraints = new GridBagConstraints(0, 9, 1, 1, 0, 0,
+            GridBagConstraints communicationConstraints = new GridBagConstraints(0, 12, 1, 1, 0, 0,
                     GridBagConstraints.NORTHWEST, GridBagConstraints.NONE, new Insets(0, 5, 0, 0), 0, 0);
             filterMenu.add(communicationCheckBox, communicationConstraints);
         }
@@ -183,30 +209,50 @@ public class ApplicantListPage extends JPanel{
 
             editApplicantButton = new JButton(new ImageIcon(ImageBase64.base64ToImage(ImageEmbedded.EDIT_ICON)));
             editApplicantButton.setBorder(BorderFactory.createEmptyBorder());
+            editApplicantButton.setEnabled(false);
             GridBagConstraints editApplicantConstraints = new GridBagConstraints(0, 1, 1, 1, 1, 0,
                     GridBagConstraints.NORTH, GridBagConstraints.NONE, new Insets(0, 0, 0, 0), 0, 0);
             buttonPanel.add(editApplicantButton, editApplicantConstraints);
 
-            removeApplicantButton=new JButton(new ImageIcon((ImageBase64.base64ToImage(ImageEmbedded.REMOVE_ICON)).getScaledInstance(60,60,Image.SCALE_SMOOTH)));
+            removeApplicantButton=new JButton(new ImageIcon((ImageBase64.base64ToImage(ImageEmbedded.REMOVE_ICON))));
             removeApplicantButton.setBorder(BorderFactory.createEmptyBorder());
+            removeApplicantButton.setEnabled(false);
             GridBagConstraints removeApplicantConstraints = new GridBagConstraints(0, 2, 1, 1, 1, 0,
                     GridBagConstraints.NORTH, GridBagConstraints.NONE, new Insets(0, 0, 0, 0), 0, 0);
             buttonPanel.add(removeApplicantButton, removeApplicantConstraints);
 
             setInterviewDateButton = new JButton(new ImageIcon(ImageBase64.base64ToImage(ImageEmbedded.SET_INTERVIEW_ICON)));
             setInterviewDateButton.setBorder(BorderFactory.createEmptyBorder());
-            GridBagConstraints setInterviewDateConstraints = new GridBagConstraints(0, 3, 1, 1, 1, 1,
+            setInterviewDateButton.setEnabled(false);
+            GridBagConstraints setInterviewDateConstraints = new GridBagConstraints(0, 3, 1, 1, 1, 0,
                     GridBagConstraints.NORTH, GridBagConstraints.NONE, new Insets(0, 0, 0, 0), 0, 0);
             buttonPanel.add(setInterviewDateButton, setInterviewDateConstraints);
 
+            acceptApplicantButton = new JButton(new ImageIcon(ImageBase64.base64ToImage(ImageEmbedded.ACCEPT_ICON)));
+            acceptApplicantButton.setBorder(BorderFactory.createEmptyBorder());
+            acceptApplicantButton.setToolTipText("Accept Applicant");
+            acceptApplicantButton.setEnabled(false);
+            GridBagConstraints acceptApplicantConstraints = new GridBagConstraints(0, 4, 1, 1, 1, 1,
+                    GridBagConstraints.NORTH, GridBagConstraints.NONE, new Insets(0, 0, 0, 0), 0, 0);
+            buttonPanel.add(acceptApplicantButton, acceptApplicantConstraints);
+
         }
         if (main.getController().getUser().authorised(Staff.MANAGER)){
-            shortlistApplicantButton = new JButton(new ImageIcon(ImageBase64.base64ToImage(ImageEmbedded.ADD_TO_SHORTLIST_ICON)));
 
+            shortlistApplicantButton = new JButton(new ImageIcon(ImageBase64.base64ToImage(ImageEmbedded.ADD_TO_SHORTLIST_ICON)));
             shortlistApplicantButton.setBorder(BorderFactory.createEmptyBorder());
-            GridBagConstraints shortlistApplicantConstraints = new GridBagConstraints(0, 0, 1, 1, 1, 1,
+            shortlistApplicantButton.setEnabled(false);
+            GridBagConstraints shortlistApplicantConstraints = new GridBagConstraints(0, 0, 1, 1, 1, 0,
                     GridBagConstraints.NORTH, GridBagConstraints.NONE, new Insets(0, 0, 0, 0), 0, 0);
             buttonPanel.add(shortlistApplicantButton, shortlistApplicantConstraints);
+
+            setJobRoleButton = new JButton(new ImageIcon(ImageBase64.base64ToImage(ImageEmbedded.SET_JOB_ROLE_ICON)));
+            setJobRoleButton.setBorder(BorderFactory.createEmptyBorder());
+            setJobRoleButton.setToolTipText("Accept Applicant");
+            setJobRoleButton.setEnabled(false);
+            GridBagConstraints setJobRoleConstraints = new GridBagConstraints(0, 1, 1, 1, 1, 1,
+                    GridBagConstraints.NORTH, GridBagConstraints.NONE, new Insets(0, 0, 0, 0), 0, 0);
+            buttonPanel.add(setJobRoleButton, setJobRoleConstraints);
         }
 
         GridBagConstraints buttonPanelConstraints = new GridBagConstraints(1, 1, 1, 1, 0, 0,
@@ -237,7 +283,7 @@ public class ApplicantListPage extends JPanel{
 
         });
 
-        waitingShortlist.addActionListener(e -> updateModel());
+        waitingShortlistCheckBox.addActionListener(e -> updateModel());
 
         shortlistedCheckBox.addActionListener(e -> updateModel());
 
@@ -277,32 +323,78 @@ public class ApplicantListPage extends JPanel{
                 if (selectedRow == -1) {
                     return;
                 }
-                if (main.getController().getApplicants()[selectedRow].getStatus() == Applicant.SHORTLISTED){
-
+                if (main.getController().getApplicantAt(selectedRow).getStatus() == Applicant.SHORTLISTED_PENDING_DATE){
+                    main.showApplicantKeyPage(Applicant.SHORTLISTED_PENDING_DATE, selectedRow);
                 }
             });
 
+            acceptApplicantButton.addActionListener(e -> {
+                int selectedRow = table.convertRowIndexToModel(table.getSelectedRow());
+                main.getController().acceptApplicant(selectedRow);
+                updateModel();
+                acceptApplicantButton.setEnabled(false);
+            });
+
+            table.getSelectionModel().addListSelectionListener(e -> {
+                if (table.getSelectedRow() != -1) {
+                    int status = main.getController().getApplicantAt(table.convertRowIndexToModel(table.getSelectedRow())).getStatus();
+                    editApplicantButton.setEnabled(true);
+                    setInterviewDateButton.setEnabled(status == Applicant.SHORTLISTED_PENDING_DATE);
+                    acceptApplicantButton.setEnabled(status == Applicant.SHORTLISTED_TO_INTERVIEW);
+                    removeApplicantButton.setEnabled(true);
+                }
+                else {
+                    editApplicantButton.setEnabled(false);
+                    setInterviewDateButton.setEnabled(false);
+                    acceptApplicantButton.setEnabled(false);
+                    removeApplicantButton.setEnabled(false);
+                }
+            });
         }
         if (main.getController().getUser().authorised(Staff.MANAGER)) {
             shortlistApplicantButton.addActionListener(e -> {
                 int selectedRow = table.convertRowIndexToModel(table.getSelectedRow());
-                main.getController().shortlistApplicant(selectedRow);
+                main.getController().shortlistApplicantForInterview(selectedRow);
+                updateModel();
+                shortlistApplicantButton.setEnabled(false);
+            });
+            setJobRoleButton.addActionListener(e -> {
+                int selectedRow = table.convertRowIndexToModel(table.getSelectedRow());
+                if (selectedRow == -1) {
+                    return;
+                }
+                if (main.getController().getApplicantAt(selectedRow).getStatus() == Applicant.ACCEPTED_WAITING_JOB){
+                    main.showApplicantKeyPage(Applicant.ACCEPTED_WAITING_JOB, selectedRow);
+                }
+            });
+
+            table.getSelectionModel().addListSelectionListener(e -> {
+                if (table.getSelectedRow() != -1) {
+                    int status = main.getController().getApplicantAt(table.convertRowIndexToModel(table.getSelectedRow())).getStatus();
+                    setJobRoleButton.setEnabled(status == Applicant.ACCEPTED_WAITING_JOB);
+                    shortlistApplicantButton.setEnabled(status == Applicant.WAITING_SHORTLIST);
+                }
+                else {
+                    setJobRoleButton.setEnabled(false);
+                    shortlistApplicantButton.setEnabled(false);
+                }
             });
         }
+
     }
 
     private void updateModel(){
-        List<Integer> shortlistStages = new ArrayList<>();
-        if (waitingShortlist.isSelected()) {
-            shortlistStages.add(ApplicantRowFilter.STAGE_AWAITING_SHORTLIST);
-        }
-        if (shortlistedCheckBox.isSelected()) {
-            shortlistStages.add(ApplicantRowFilter.STAGE_AWAITING_ACCEPTANCE);
-        }
-        if (acceptedCheckBox.isSelected()) {
-            shortlistStages.add(ApplicantRowFilter.STAGE_ACCEPTED);
-        }
+        List<Integer> filterStages = getFilterStages();
+        List<String> skillFilters = getExcludes();
 
+        sorter.setRowFilter(new ApplicantRowFilter(
+                filterStages.stream().mapToInt(i->i).toArray(),
+                filterField.getText(),
+                skillFilters.toArray(new String[0])));
+        table.setRowSorter(sorter);
+    }
+
+    private List<String> getExcludes() {
         List<String> skillFilters = new ArrayList<>();
         if (programmingCheckBox.isSelected()){
             skillFilters.add("Programming");
@@ -316,12 +408,27 @@ public class ApplicantListPage extends JPanel{
         if (communicationCheckBox.isSelected()){
             skillFilters.add("Communication");
         }
+        return skillFilters;
+    }
 
-        sorter.setRowFilter(new ApplicantRowFilter(
-                shortlistStages.stream().mapToInt(i->i).toArray(),
-                filterField.getText(),
-                skillFilters.toArray(new String[0])));
-        table.setRowSorter(sorter);
+    private List<Integer> getFilterStages() {
+        List<Integer> filterStages = new ArrayList<>();
+        if (waitingShortlistCheckBox.isSelected()) {
+            filterStages.add(Applicant.WAITING_SHORTLIST);
+        }
+        if (shortlistedWaitingHRCheckBox.isSelected()) {
+            filterStages.add(Applicant.SHORTLISTED_PENDING_DATE);
+        }
+        if (shortlistedCheckBox.isSelected()) {
+            filterStages.add(Applicant.SHORTLISTED_TO_INTERVIEW);
+        }
+        if (acceptedWaitingManagerCheckBox.isSelected()) {
+            filterStages.add(Applicant.ACCEPTED_WAITING_JOB);
+        }
+        if (acceptedCheckBox.isSelected()) {
+            filterStages.add(Applicant.ACCEPTED);
+        }
+        return filterStages;
     }
 
     public double getZoomFactor(){
