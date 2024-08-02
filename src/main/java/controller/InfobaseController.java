f7750ece00d8e7c08b3fa797141e200af9e5cf61
josep <joseph_chiu@Outlook.com>
2024-08-02 23:08:55 +0800

    Update InfobaseController.java: - Added import statement for javax.swing - Changed access modifiers of instance variables and added final keyword where applicable - Modified addApplicant method to include a pdfFile parameter and added print statement for debugging purposes - Updated file path separator in DataIO.writeFile to use "\\" instead of "/" - Modified setApplicantInstance method to accept an index parameter and changed its logic accordingly - Refactored applyApplicantEdits method to include a call to overwriteIndex method - Added overwriteIndex method to update applicant data in the file system - Updated getImage, getName, getNRIC, getEmail methods to retrieve data from getApplicantInstance - Added removeSkill and editSkill methods to manipulate skills in editsDataStorageInstance - Renamed shortlistApplicant method to shortlistApplicantForInterview and updated its logic to handle SHORTLISTED_PENDING_DATE status - Added confirmApplicantShortlist and confirmApplicantJob methods to update applicant status - Refactored acceptApplicant method to handle ACCEPTED_WAITING_JOB status - Added getBirthdate, getGender, setInterviewTime, getInterviewTime, getApplicantAt, and setJobRole methods
    -Hail Steven

--- src/main/java/controller/InfobaseController.java
+++ src/main/java/controller/InfobaseController.java
@@ -1,11 +1,10 @@
 package controller;
 
 import data.*;
-import subsystems.ImageBase64;
 import subsystems.JsonReaderWriter;
 import subsystems.SHA256;
 
-import java.awt.*;
+import javax.swing.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.time.LocalDate;
@@ -15,11 +14,10 @@ import java.util.Collections;
 import java.util.Locale;
 
 public class InfobaseController {
-    Applicant applicantInstance;
-    Staff loggedInStaffInstance;
-    EditsDataStorage editsDataStorageInstance;
-    ApplicantDataStorage applicantDataStorage;
-    int index;
+    private final Staff loggedInStaffInstance;
+    private EditsDataStorage editsDataStorageInstance;
+    private final ApplicantDataStorage applicantDataStorage;
+    private int index;
 
     public InfobaseController(Staff loggedInStaffInstance) {
         this.loggedInStaffInstance = loggedInStaffInstance;
@@ -40,14 +38,15 @@ public class InfobaseController {
     }
 
 
-    public void addApplicant(String name, int day, String month, int year,String email, String nric, String gender, String base64Img, String[] skills){
+    public void addApplicant(String name, int day, String month, int year,String email, String nric, String gender, String base64Img, String[] skills, String pdfFile){
         String birthString = String.valueOf(day) + ' ' + month + ' ' + year;
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("d MMMM yyyy", Locale.ENGLISH);
         LocalDate birthDate = LocalDate.parse(birthString, formatter);
-        Applicant applicant=new Applicant(name, birthDate.toEpochDay(), LocalDate.now().getYear()-birthDate.getYear(), email, nric, gender, base64Img, skills);
+        Applicant applicant=new Applicant(name, birthDate.toEpochDay(), LocalDate.now().getYear()-birthDate.getYear(), email, nric, gender, base64Img, skills, pdfFile);
         applicantDataStorage.addApplicant(applicant);
+        System.out.println("applicant added");
         File applicantDirectory = new File(System.getProperty("user.dir") + "\\" + "applicants");
-        DataIO.writeFile(applicantDirectory + new String(SHA256.getHasherHex().hashString(String.valueOf(System.nanoTime()))) + "_Applicant.json", JsonReaderWriter.modelToJson(applicant));
+        DataIO.writeFile(applicantDirectory + "\\" + new String(SHA256.getHasherHex().hashString(String.valueOf(System.nanoTime()))) + "_Applicant.json", JsonReaderWriter.modelToJson(applicant));
     }
 
     public void removeApplicant(int index){
@@ -61,28 +60,33 @@ public class InfobaseController {
 
     }
 
-    public void setApplicantInstance(Applicant applicant, int index){
+    // CALL THIS METHOD BEFORE INTERFACING WITH DATA METHODS
+    public void setApplicantInstance(int index){
         this.index = index;
-        applicantInstance = applicant;
-        editsDataStorageInstance = new EditsDataStorage(applicant);
+        editsDataStorageInstance = new EditsDataStorage(getApplicantInstance());
+    }
 
-
+    public Applicant getApplicantInstance(){
+        return applicantDataStorage.getApplicantAt(index);
     }
 
     public void applyApplicantEdits(String name, int day, String month, int year, String nricFin, String email, String gender, String s){
         String dateString = String.valueOf(day) + ' ' + month + ' ' + year;
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("d MMMM yyyy", Locale.ENGLISH);
         LocalDate localDate = LocalDate.parse(dateString, formatter);
-        Applicant edittedApplicant = new Applicant(name, localDate.toEpochDay(), LocalDate.now().getYear()-localDate.getYear(), email, nricFin, gender, s, editsDataStorageInstance.getSkills());
-        edittedApplicant.copyApplicantMetadata(applicantInstance);
+        Applicant edittedApplicant = new Applicant(name, localDate.toEpochDay(), LocalDate.now().getYear()-localDate.getYear(), email, nricFin, gender, s, editsDataStorageInstance.getSkills(), getApplicantInstance());
         applicantDataStorage.editApplicant(index, edittedApplicant);
+        overwriteIndex(index, edittedApplicant);
+    }
+
+    private void overwriteIndex(int index, Applicant newApplicant){
         File applicantDirectory = new File(System.getProperty("user.dir") + "\\" + "applicants");
         File[] contents = applicantDirectory.listFiles();
         if (contents != null) {
             Collections.reverse(Arrays.asList(contents));
             File targetApplicantFile = contents[index];
             if (targetApplicantFile.delete()) {
-                DataIO.writeFile(targetApplicantFile.getAbsolutePath(), JsonReaderWriter.modelToJson(edittedApplicant));
+                DataIO.writeFile(targetApplicantFile.getAbsolutePath(), JsonReaderWriter.modelToJson(newApplicant));
             }
             else {
                 System.out.println("Can't delete file");
@@ -91,18 +95,18 @@ public class InfobaseController {
     }
 
     public BufferedImage getImage() {
-        return editsDataStorageInstance.getImage();
+        return getApplicantInstance().getImage();
     }
     public String getName() {
-        return editsDataStorageInstance.getName();
+        return getApplicantInstance().getName();
     }
 
     public String getNRIC() {
-        return editsDataStorageInstance.getNric();
+        return getApplicantInstance().getNRIC();
     }
 
     public String getEmail() {
-        return editsDataStorageInstance.getEmail();
+        return getApplicantInstance().getEmail();
     }
 
     public String[] getSkills() {
@@ -113,6 +117,14 @@ public class InfobaseController {
         editsDataStorageInstance.addSkill(editSkillTextField);
     }
 
+    public void removeSkill(int selectedIndex) {
+        editsDataStorageInstance.removeSkill(selectedIndex);
+    }
+
+    public void editSkill(int index, String editedSkill) {
+        editsDataStorageInstance.editSkill(index, editedSkill);
+    }
+
     public Applicant[] getApplicants() {
         return applicantDataStorage.getApplicants();
     }
@@ -121,17 +133,58 @@ public class InfobaseController {
         return loggedInStaffInstance;
     }
 
-    public void shortlistApplicant(int selectedRow) {
-        Applicant applicantsToShortlist = applicantDataStorage.getApplicants()[selectedRow];
-        applicantsToShortlist.setStatus(Applicant.SHORTLISTED);
+    public void shortlistApplicantForInterview(int selectedRow) {
+        setApplicantInstance(selectedRow);
+        Applicant applicantsToShortlist = applicantDataStorage.getApplicantAt(index);
+        applicantsToShortlist.setStatus(Applicant.SHORTLISTED_PENDING_DATE);
+        applicantDataStorage.editApplicant(index, applicantsToShortlist);
+        overwriteIndex(index, applicantsToShortlist);
+    }
+
+    public void confirmApplicantShortlist(){
+        Applicant applicant = getApplicantInstance();
+        applicant.setStatus(Applicant.SHORTLISTED_TO_INTERVIEW);
+        overwriteIndex(index, applicant);
+    }
+
+    public void confirmApplicantJob(){
+        Applicant applicant = getApplicantInstance();
+        applicant.setStatus(Applicant.ACCEPTED);
+        overwriteIndex(index, applicant);
     }
 
     public void acceptApplicant(int selectedRow) {
-        Applicant applicantToAccept = applicantDataStorage.getApplicants()[selectedRow];
-        applicantToAccept.setStatus(Applicant.ACCEPTED);
+        setApplicantInstance(selectedRow);
+        Applicant applicantToAccept = applicantDataStorage.getApplicantAt(index);
+        applicantToAccept.setStatus(Applicant.ACCEPTED_WAITING_JOB);
+        applicantDataStorage.editApplicant(index, applicantToAccept);
+        overwriteIndex(index, applicantToAccept);
     }
 
     public String getBirthdate() {
-        return editsDataStorageInstance.getBirthdate();
+        return getApplicantInstance().getBirthdate();
+    }
+
+    public String getGender() {
+        return getApplicantInstance().getGender();
+    }
+
+    public void setInterviewTime(long time) {
+        Applicant applicantToInterview = applicantDataStorage.getApplicantAt(index);
+        applicantToInterview.setInterviewTime(time);
+        overwriteIndex(index, applicantToInterview);
+    }
+
+    public String getInterviewTime(){
+        return getApplicantInstance().getInterviewTime();
+    }
+
+    public Applicant getApplicantAt(int index) {
+        return applicantDataStorage.getApplicantAt(index);
+    }
+
+    public void setJobRole(String applicantAssignedField) {
+        Applicant applicantToAssign = getApplicantInstance();
+        applicantToAssign.setJobRole(applicantAssignedField);
     }
 }
