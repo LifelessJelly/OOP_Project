28f44d093704cb055c71c373575595cc8b971556
onath_li <onathan.v.2@gmail.com>
2024-08-03 11:50:35 +0800

    Added the extra details in SummaryPage

--- src/main/java/gui/infobase/SummaryPage.java
+++ src/main/java/gui/infobase/SummaryPage.java
@@ -9,37 +9,46 @@ import javax.swing.table.TableColumn;
 import javax.swing.table.TableModel;
 import javax.swing.table.TableRowSorter;
 import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 public class SummaryPage extends JPanel {
     InfobaseMainframe main;
+    private JLabel summaryListLabel;
+    private TableModel applicantModel;
+    private JTable table;
+    private TableColumn pictureColumn;
+    private TableColumn infoColumn;
+    private JScrollPane tableScroll;
 
     public SummaryPage(InfobaseMainframe main) {
         this.main = main;
         this.setLayout(new GridBagLayout());
 
         initComponent();
+        initListeners();
     }
 
     private void initComponent() {
-        JLabel summaryListLabel = new JLabel("Applicant Summary");
+        summaryListLabel = new JLabel("Applicant Summary");
         summaryListLabel.setFont(summaryListLabel.getFont().deriveFont(20f));
         GridBagConstraints summaryListConstraints = new GridBagConstraints(0, 0, 1, 1, 0, 0,
                 GridBagConstraints.CENTER, GridBagConstraints.BOTH, new Insets(0, 0, 0, 0), 0, 0);
         this.add(summaryListLabel, summaryListConstraints);
 
-        TableModel applicantModel = new ApplicantTableModel(main);
-        JTable table = new JTable(applicantModel);
+        applicantModel = new ApplicantTableModel(main);
+        table = new JTable(applicantModel);
         TableRowSorter<TableModel> sorter = new TableRowSorter<>(applicantModel);
         //TableRowSorter uses applicantModel as underlying model
         table.setRowSorter(sorter);
         table.setMinimumSize(new Dimension(700, 300));
         table.getTableHeader().setUI(null);
-        TableColumn pictureColumn = table.getColumnModel().getColumn(0);
-        TableColumn infoColumn = table.getColumnModel().getColumn(1);
+        pictureColumn = table.getColumnModel().getColumn(0);
+        infoColumn = table.getColumnModel().getColumn(1);
         pictureColumn.setPreferredWidth(120);
         pictureColumn.setMaxWidth(120);
 
-        JScrollPane tableScroll=new JScrollPane(table);
+        tableScroll=new JScrollPane(table);
         tableScroll.setMinimumSize(new Dimension(700, 300));
         tableScroll.setPreferredSize(new Dimension(700, 300));
         tableScroll.setMaximumSize(new Dimension(getWidth(),(int)(Math.round(0.7*getHeight()))));
@@ -65,4 +74,49 @@ public class SummaryPage extends JPanel {
 
         summaryListLabel.setText("Applicant Summary- "+(sorter.getViewRowCount())+" applicants accepted");
     }
+
+    private void initListeners(){
+        table.addMouseListener(new MouseAdapter() {
+
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                int selectedRow = table.convertRowIndexToModel(table.getSelectedRow());
+                Applicant selectedApplicant = main.getController().getApplicantAt(selectedRow);
+                if (e.getClickCount() == 2) {
+                    JDialogSpawner(selectedApplicant);
+                }
+            }
+        });
+    }
+    //TODO:ADD TRANSLATION
+    private void JDialogSpawner(Applicant applicantInstance){
+        JDialog extraDialog = new JDialog();
+
+        extraDialog.setTitle("Applicant extra info");
+        extraDialog.setSize(300,300);
+        extraDialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+        JTextArea textArea = new JTextArea();
+        textArea.setEditable(false);
+        textArea.setAutoscrolls(true);
+        textArea.setBorder(BorderFactory.createLineBorder(Color.getHSBColor(166,14.8F,55.7F)));
+        textArea.setFont(textArea.getFont().deriveFont(18f));
+        textArea.setLineWrap(true);
+
+        textArea.setText(main.getLocale("ApplicantListPage.JTable.applicantName")+applicantInstance.getName()+"\n"+
+                        main.getLocale("ApplicantListPage.JTable.applicantBirthDate")+applicantInstance.getBirthdate()+"\n"+
+                        main.getLocale("ApplicantListPage.JTable.applicantAge")+applicantInstance.getAge()+'\n'+
+                        main.getLocale("ApplicantListPage.JTable.applicantNRIC")+applicantInstance.getNRIC()+'\n'+
+                        main.getLocale("ApplicantListPage.JTable.applicantEmail")+applicantInstance.getEmail()+'\n'+
+                        main.getLocale("ApplicantListPage.JTable.applicantGender")+applicantInstance.getGender()+'\n'+
+                        main.getLocale("ApplicantListPage.JTable.applicantSkills") + String.join(", ", applicantInstance.getSkills())
+        );
+
+        JScrollPane txtAreaScroll = new JScrollPane();
+        txtAreaScroll.setViewportView(textArea);
+        txtAreaScroll.setAutoscrolls(true);
+        extraDialog.add(txtAreaScroll);
+        extraDialog.setVisible(true);
+      }
 }
+
+
