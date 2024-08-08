600ba07f96ca8a51a56610af5446567fe7843b07
josep <joseph_chiu@Outlook.com>
2024-08-09 01:11:19 +0800

    data class for email credentials

---
+++ src/main/java/data/EmailCredential.java
@@ -0,0 +1,19 @@
+package data;
+
+public class EmailCredential {
+    String username;
+    String password;
+
+    public EmailCredential(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}
