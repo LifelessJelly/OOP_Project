40d17e8b58ac1ce827bada04ac6cc8ed4bf6a8f1
josep <joseph_chiu@outlook.com>
2024-08-11 12:33:36 +0800

    removed comment block

--- src/main/java/controller/Main.java
+++ src/main/java/controller/Main.java
@@ -1,6 +1,7 @@
 package controller;
 
 
+
 public class Main {
 
 
@@ -9,36 +10,6 @@ public class Main {
 
 
         new LoginMainframe();
-//
-//        Image image1;
-////        Image image2;
-////        Image image3;
-////        Image image4;
-////        Image image5;
-////        Image image6;
-////        Image image7;
-////        Image image8;
-//        try {
-//            image1 = ImageIO.read(new File("C:\\Users\\User\\IdeaProjects\\OOP_Project_real\\ProfileIcon.png"));
-////            image2 = ImageIO.read(new File("E:\\IDEAProjects\\OOP\\CompanyLogoThemes\\CompanyLogoPornhub.png"));
-////            image3 = ImageIO.read(new File("E:\\IDEAProjects\\OOP\\CompanyLogoThemes\\CompanyLogoNostalgia.png"));
-////            image4 = ImageIO.read(new File("E:\\IDEAProjects\\OOP\\CompanyLogoThemes\\CompanyLogoNight.png"));
-////            image5 = ImageIO.read(new File("E:\\IDEAProjects\\OOP\\CompanyLogoThemes\\CompanyLogoMint.png"));
-////            image6 = ImageIO.read(new File("E:\\IDEAProjects\\OOP\\CompanyLogoThemes\\CompanyLogoTide.png"));
-////            image7 = ImageIO.read(new File("E:\\IDEAProjects\\OOP\\CompanyLogoThemes\\CompanyLogoUnicorn.png"));
-////            image8 = ImageIO.read(new File("E:\\IDEAProjects\\OOP\\AddToShortlistIcon.png"));
-//        } catch (IOException e) {
-//            throw new RuntimeException(e);
-//        }
-////
-//        System.out.println(ImageBase64.imageToBase64(image1));
-//        System.out.println(ImageBase64.imageToBase64(image2));
-//        System.out.println(ImageBase64.imageToBase64(image3));
-//        System.out.println(ImageBase64.imageToBase64(image4));
-//        System.out.println(ImageBase64.imageToBase64(image5));
-//        System.out.println(ImageBase64.imageToBase64(image6));
-//        System.out.println(ImageBase64.imageToBase64(image7));
-//        System.out.println(ImageBase64.imageToBase64(image8));
     }
 }
 
